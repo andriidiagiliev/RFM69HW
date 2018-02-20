23691f0017e5452272417c4aa7a055f2acb0fb8b
Digital <dyagilev@gmail.com>
2018-02-21 01:24:36 +0200

    add ANDRÉS BLOG code

---
+++ hw_config.h
@@ -0,0 +1,19 @@
+﻿/**
+ * @file hw_config.h
+ *
+ * @brief HAL configuration
+ * @date January 2015
+ * @author André Heßling
+ *
+ * Include the header files that are needed for the controller.
+ */
+ 
+#ifndef HW_CONFIG_H_
+#define HW_CONFIG_H_
+
+#include "stm32l1xx.h"
+
+// Include this for STM32F0 devices
+//#include "stm32f0xx.h"
+
+#endif /* HW_CONFIG_H_ */
